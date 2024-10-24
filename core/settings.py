b9fcf9b6cdf414077e9141880edff254329a5f65
Enamul Bokshi <133862506+EnamulBokshi@users.noreply.github.com>
2024-10-24 13:52:26 +0600

    changed dirs at templates

--- core/settings.py
+++ core/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR,'dist')],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
