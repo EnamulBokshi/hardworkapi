e4b7616522ad7c6511aef7db4aa837c30faa5881
Enamul Bokshi <133862506+EnamulBokshi@users.noreply.github.com>
2024-10-27 22:02:46 +0600

    wsgi file modified

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,18 +1,11 @@
-"""
-WSGI config for core project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
 
 import os
 from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 from django.core.wsgi import get_wsgi_application
-dirs = BASE_DIR / "core.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", dirs)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
 
