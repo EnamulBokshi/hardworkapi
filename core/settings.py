4c726734a090bde2158d28b11a6a780c9b928d1b
Enamul Bokshi <133862506+EnamulBokshi@users.noreply.github.com>
2024-10-28 18:43:04 +0600

    sub domain allowed'

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,8 @@ ALLOWED_HOSTS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://184.168.113.249'
+    'http://184.168.113.249',
+    'https://abc.ancientsbuilders.com/'
 ]
 # Application definition
 
