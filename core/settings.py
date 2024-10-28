2d5de08930e6aa7f21127edbf6845bb5c7f6feec
Enamul Bokshi <133862506+EnamulBokshi@users.noreply.github.com>
2024-10-28 18:47:03 +0600

    more host allowed

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,11 @@ ALLOWED_HOSTS = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://184.168.113.249',
-    'https://abc.ancientsbuilders.com/'
+    'abc.ancientsbuilders.com',
+    'ancientsbuilders.com',
+    'www.ancientsbuilders.com',
+    'www.abc.ancientsbuilders.com',
+
 ]
 # Application definition
 
